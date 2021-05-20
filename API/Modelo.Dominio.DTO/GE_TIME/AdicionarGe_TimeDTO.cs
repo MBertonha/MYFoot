26cce3086ef4193371a28d3c3db1024d834a0bb4
Matheus Bertonha <bertonha_2011@hotmail.com>
2021-05-20 00:34:19 -0300

    Mudanças de pasta para modelo front

---
+++ API/Modelo.Dominio.DTO/GE_TIME/AdicionarGe_TimeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tnf.Dto;
+
+namespace Modelo.Dominio.DTO
+{
+    public class AdicionarGE_TimeDTO : BaseDto
+    {
+    public int SeqTime { get; set; }
+    public string NomeTime { get; set; }
+    public string CEP { get; set; }
+    public string UF { get; set; }
+    public int TipoPlano { get; set; }
+    public string Ativo { get; set; }
+    public DateTime DataInclusao { get; set; }
+    }
+}
